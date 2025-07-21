0bf5cd95e1e765dbb0fe0a0010fa272bd34938d2
anshu7974444 <anshuthakur7974@gmail.com>
2025-07-21 06:41:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = 'money-tool-v1';
+const appShellFiles = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(appShellFiles))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
